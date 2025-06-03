2a48251869b0ee98c6fdb094f7e1406d4f70d841
ganesh shelke <shelke.ganesh87@gmail.com>
2025-06-03 15:59:14 +0530

    added dev1.1 fn

--- math.js
+++ math.js
@@ -10,6 +10,12 @@ const subtract = (a, b) => {
 const multiply = (a, b) => {
     return a * b;
 }   
+
+// div 1.1 function added into the math.js file.
+const  div = (a,b) =>{
+    return a/b;
+} 
+
 // add div function
 
 /// created new dev1.1 branch
